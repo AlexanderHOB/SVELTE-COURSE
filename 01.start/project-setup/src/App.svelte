b6a9c0a402c8763bd0a9fd239a299c7e99b51f4f
AlexanderHOB <obispoa1@gmail.com>
2022-04-23 22:53:09 -0500

    Module 1: Base Syntax & Core Features

---
+++ 01.start/project-setup/src/App.svelte
@@ -0,0 +1,74 @@
+<script>
+	import ContactCard from './ContactCard.svelte';
+	export let userName;
+	let titleJob = 'System Engineer';
+	let description='A like to develop applications with latest technologies';
+	let userImageUrl = 'https://i.pinimg.com/736x/8b/16/7a/8b167af653c2399dd93b952a48740620.jpg';
+	let userSkills = 'Svelte, Javascript, Node, Ruby, Go, React, Express, Deno, Python';
+
+</script>
+
+<style>
+	:global(body){
+		background-color: #293241;
+		color:#E0FBFC;
+		transition: background-color 0.3s;
+		display: flex;
+		align-items: center;
+	}
+	.container {
+		max-width: 90%;
+		margin:0 auto;
+		display:flex;
+		justify-content: center;
+	}
+	.contact__section{
+		width: 50%;
+	}
+	.contact__section .input-group{
+		width: 100%;
+	}
+	.input-group input, .input-group textarea {
+		border: none;
+		border-bottom: 2px solid #E0FBFC;
+		outline: none;
+		width: 80%;
+		background-color: transparent;
+		color: #EE6C4D;
+		font-weight: 700;
+		padding: 50 10px;
+	}
+	.input-group textarea {
+		resize: none;
+	}
+</style>
+<div class="container">
+	
+	
+	<section class="contact__section">
+		<div class="title__container">
+			<h1>Data Contact Card</h1>
+		</div>
+		<div class="input-group">
+			<label for="username">Name: </label>
+			<input type='text' class='form-control' bind:value={userName} autocomplete="off" id="username"/>
+		</div>
+		<div class="input-group">
+			<label for="titleJob">Job: </label>
+			<input type='text' class='form-control' bind:value={titleJob} id="titleJob" />
+		</div>
+		<div class="input-group">
+			<label for="userImageUrl">Image Url: </label>
+			<input type='text' class='form-control' bind:value={userImageUrl} id="userImageUrl"/>
+		</div>
+		<div class="input-group">
+			<label for="userSkills">Skills: </label>
+			<input type='text' class='form-control' bind:value={userSkills} id="userSkills"/>
+		</div>
+		<div class="input-group">
+			<label for="description">Description: </label>
+			<textarea name='text' class='form-control' bind:value={description} rows="3" id="description"></textarea>
+		</div>
+	</section>
+	<ContactCard {userName} {userImageUrl} {description} {titleJob} {userSkills}/>
+</div>
---
+++ 01.start/project-setup/src/ContactCard.svelte
@@ -0,0 +1,92 @@
+<script>
+    export let userName;
+    export let titleJob;
+    export let userImageUrl;
+    export let description;
+    export let userSkills;
+</script>
+
+<style>
+    .contact-card {
+        box-shadow: -10px 10px 8px rgba(0, 0, 0, 0.26);
+        width: 50%;
+        border-radius: 15px;
+        margin: 1rem auto;
+    }
+
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 8rem;
+    }
+
+    .thumb {
+        width: 33%;
+        height: 100%;
+    }
+
+    .thumb-placeholder {
+        background-color:#ccc;
+    }
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+
+    .user-data {
+        width: 67%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    h1 {
+        font-size: 1.25rem;
+        font-family: "Roboto Slab", sans-serif;
+        margin: 0.5rem 0;
+    }
+
+    h2 {
+        font-size: 1rem;
+        font-weight: normal;
+        color: #E0FBFC;
+        margin: 0;
+        margin-bottom: 0.5rem;
+    }
+
+    .description__section {
+        max-height: 100%;
+        display:flex;
+        flex-flow: row wrap;
+        align-items: space-around;
+        border-top: 1px solid #EE6C4D;
+        padding: 20px;
+    }
+    .description__section-skills{
+        font-size: 12px;
+        color:#98C1D9;
+    }
+    .description__section-summary{
+        font-size: 1 rem;
+    }
+</style>
+
+<div class="contact-card">
+    <header>
+        <div class="thumb" class:thumb-placeholder={!userImageUrl}>
+            <img src="{userImageUrl}" alt="" />
+        </div>
+        <div class="user-data">
+            <h1>{userName}</h1>
+            <h2>{titleJob}</h2>
+        </div>
+    </header>
+    <div class="description__section">
+        <p class="description__section-summary">{description}</p>
+        <p class="description__section-skills">{userSkills}</p>
+    </div>
+</div>
